1e336c4dfd037c5bbf290fc57eea8025b98f13b5
aditorito <66251570+aditorito@users.noreply.github.com>
2023-09-21 12:30:50 +0000

    converstion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hello world"
+
+let booleanIsLogged = Boolean(isLoggedIn)
+// console.log(booleanIsLogged);
+
+// 1 => true; 0 => false
+// "" => false
+// "anytext" => true
+
+
+let num = 33
+let stringnumber = String(num)
+log
+console.log(stringnumber);
+
