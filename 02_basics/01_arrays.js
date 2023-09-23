5c6ac355cd3e4feaad57eab364ef07c7a349ef29
aditorito <66251570+aditorito@users.noreply.github.com>
2023-09-23 12:09:51 +0000

    01 arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4, 5, 6, true, "bholu"]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+// console.log(myArr[5]);
+
+// Array methods 
+
+// myArr.push(69)
+// myArr.pop()
+
+// myArr.unshift(9) insert in the start of array
+// myArr.shift() removes the firat element
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(true));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+// slice , splice
+
+// console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 5)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 5);
+// console.log("C ", myArr);
+// console.log(myn2);
+
+
