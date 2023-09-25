b17a22587afac3ee0b1703730741844923fa7ece
aditorito <66251570+aditorito@users.noreply.github.com>
2023-09-25 16:10:57 +0000

    pre function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,18 @@
+function sayMyname(){
+    console.log('bholu');
+}
+
+// sayMyname();
+
+// function addTwoNumbers(num1 , num2){
+//     console.log(num1 + num2);
+// }
+
+function addTwoNumbers(num1 , num2){
+    let result = 
+}
+
+
+addTwoNumbers(3,5);
+
+// num1 and num2 are called parameters and 3 and 5 are arguments
