132c3485dd6e880539d0864ff932ddf87b85b03f
Aditya Pratap Singh <66251570+aditorito@users.noreply.github.com>
2024-01-30 16:31:27 +0000

    X100 trying completed

---
+++ x100/First_lecture_1.2/main.js
@@ -0,0 +1,24 @@
+console.log("hello world")
+
+const arr = [1,2,3,4,5,6,7,8,9]
+// let bigNum = 0;
+// for (let i = 0; i< arr.length; i++) {
+//   if (arr[i] > bigNum) {
+//     bigNum = arr[i];
+
+//   }
+  
+//     }
+//     console.log(bigNum);
+
+// let i = 0;
+// let j = arr.length -  1;
+// let temp;
+// while(i < j){
+//     temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++;
+// }
+
+// console.log(arr);
